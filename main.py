4f1a30ebbc9e6b122a1c682c8d8e680f782b12b6
nukxkubi <130269237+nukxkubi@users.noreply.github.com>
2025-02-07 14:15:34 +1100

    Adding a config and session

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
+from distutils.command.config import config
+from random import randint
+from docutils.nodes import transition
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from secrets import token_hex
-
+from datetime import date, datetime
 from kivymd.uix.button import MDButton, MDButtonText
-from kivymd.uix.dialog import MDDialogIcon, MDDialog, MDDialogHeadlineText, MDDialogSupportingText, MDDialogButtonContainer
+from kivymd.uix.dialog import MDDialogIcon, MDDialog, MDDialogHeadlineText, MDDialogSupportingText, \
+    MDDialogButtonContainer
 from kivymd.uix.widget import MDWidget
-
-session = {
-    'login': '',
-    'token':''
-}
+from kivymd.uix.transition import MDSwapTransition, MDSlideTransition, MDFadeSlideTransition, MDSharedAxisTransition
 
 class MainApp(MDApp):
+    dialog = None
+
+    def build_config(self, config):
+        config.setdefaults('THEMING',{
+            'theme_style':'Light',
+            'primary_palette':'Green'
+        })
+
+        config.setdefaults('SESSION',{
+            'login':None,
+            'token':None,
+            'last_login':None
+        })
+        pass
+
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Aqua"
+        self.theme_cls.theme_style = self.config.get('THEMING','theme_style')
+        self.theme_cls.primary_palette = self.config.get('THEMING','primary_palette')
         self.root = Builder.load_file("ui.kv")
+        self.root.transition = MDSharedAxisTransition(
+            transition_axis= 2,
+            duration = .25
+        )
+        
+        if self.config.get('SESSION','token') != 'None' and self.check_session():
+            self.root.current('main')
         return self.root
-
-    def radio_change_color(self, checkbox, active):
-        if active:
-            self.theme_cls.primary_palette  = checkbox.value
-
-    def add_dialog(self, type_d:str = 'error', icon:str = None, title:str = None, text:str = None, ):
+        
+    def start_game(self):
+        self.root.current='game'
+        
+    def add_dialog(self, type_d:str='error', title:str=None, text:str=None, icon:str=None):
         match type_d:
-            case 'error':
+            case "error":
                 self.dialog = MDDialog(
                     MDDialogIcon(
                         icon=icon if icon else 'alert',
                         theme_font_size='Custom',
-                        font_size='50sp'
+                        font_size='32sp'
                     ),
                     MDDialogHeadlineText(
                         text=title if title else ''
@@ -39,22 +60,20 @@ class MainApp(MDApp):
                     ),
                     MDDialogButtonContainer(
                         MDWidget(),
-                        MDButton(
-                            MDButtonText(
-                                text="ОК"
-                            ),
-
-                            on_press=lambda *args: self.dialog.dismiss()
-                        )
+                        MDButton(MDWidget(),
+                                 MDButtonText(
+                                     text="ОК"
+                                 ),
+                                 on_press=lambda *args: self.dialog.dismiss()
+                                 )
                     )
                 )
-
-            case 'msg':
+            case "msg":
                 self.dialog = MDDialog(
                     MDDialogIcon(
                         icon=icon if icon else 'information-outline',
                         theme_font_size='Custom',
-                        font_size='50sp'
+                        font_size='32sp'
                     ),
                     MDDialogHeadlineText(
                         text=title if title else ''
@@ -64,43 +83,74 @@ class MainApp(MDApp):
                     ),
                     MDDialogButtonContainer(
                         MDWidget(),
-                        MDButton(
-                            MDButtonText(
-                                text="ОК"
-                            ),
-
-                            on_press=lambda *args: self.dialog.dismiss()
-                        )
+                        MDButton(MDWidget(),
+                                 MDButtonText(
+                                     text="ОК"
+                                 ),
+                                 on_press=lambda *args: self.dialog.dismiss()
+                                 )
                     )
                 )
-
         self.dialog.open()
 
+    def radio_change_color(self,checkbox,active):
+        if active:
+            self.theme_cls.primary_palette = checkbox.value
+            self.config.set('THEMING','primary_palette', checkbox.value)
+            self.config.write()
+
     def app_login(self, login, password):
-        global session
         print('Кнопка нажата!')
-
-        if login == '1' and password == '1':
-            #Авторизация успешна
-            session['login'] = login
-            session['token'] = token_hex(16)
+        if login == 'a' and password == 'a':
+            self.config.set('SESSION','login',login)
+            self.config.set('SESSION','token',token_hex())
+            self.config.set('SESSION','last_login',str(datetime.now()))
+            self.config.write()
             self.root.current = 'main'
-            self.add_dialog(type_d='msg', title='Добро пожаловать', text=f'Рады вас видеть,{login}')
+            self.add_dialog(type_d='msg',title=f'Добро пожаловать,{login}', text=f'ЕСЛИ ЧО ЗАХОДИ')
         else:
-            self.add_dialog(title='Ошибка', text='Неверный логин или пароль')
+            self.add_dialog(title='Авторизация!', text='Неправильный логи или пароль!')
+            self.root.ids.login_login=''
+            self.root.ids.login_password=''
+            self.dialog.open()
 
-    def app_logout(self, login, password, rpassword):
-        if login in ['a', 'log', 'admin', 'root']:
-            self.add_dialog(title='Регистрация', text='Данный логин уже существует!')
+    def close_session(self):
+        self.config.set('SESSION', 'login', None)
+        self.config.set('SESSION', 'password', None)
+        self.config.set('SESSION', 'last_login', None)
+        self.config.write()
+        self.root.current='Login'
+
+    def check_session(self):
+        if randint(0,2):
+            self.config.set('SESSION','last_login', None)
+            self.config.write()
+            return True          
+        else:
+            self.config.set('SESSION','login', None)
+            self.config.set('SESSION','password', None)
+            self.config.set('SESSION','last_login', None)
+            self.config.write()
+            
+    def app_logout(self,login,password,rpassword):
+        if login in ['a','b','c','']:
+            self.add_dialog(title='Ошибка регистрации', text='Пользователь существует или поле пустое!')
             return
-
-#       if mt('[0-9a-Za-z@_!]{8, 36}', password):
-#           self.add_dialog(title='Регистрация', text='Пароль не соответствует требованиям!')
-#           return
-
+        # if mt('[0-9A-Za-z@_!]{8,36}'):
+        #     self.add_dialog(title='Регистрация', text='Пользователь существует!')
+        #     return
         if rpassword != password:
-            self.add_dialog(title='Регистрация', text='Пароли не совпадают!')
+            self.add_dialog(title='Регистрация', text='Пароль не совпадает!')
             return
+            
+        self.root.ids.register_login.text = ''
+        self.root.ids.register_password.text = ''
+        self.root.ids.register_rpassword.text = ''
+        self.root.current = 'Login'
+        self.add_dialog(type_d='msg',title='УСПЕШНО', text='Вы перенаправлены на окно авторизации.')
+
+    def app_register(self, register, password):
+        pass
 
 
 MainApp().run()
